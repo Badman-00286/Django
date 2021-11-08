aa8052308c09eba3896cff11132c2fd1ed99fc2f
Maklamar <maklamar228@gmail.com>
2021-11-08 09:34:44 +0300

    fixed the error in field

--- movies/models.py
+++ movies/models.py
@@ -131,7 +131,7 @@ class Rating(models.Model):
     """Рейтинг"""
     ip = models.CharField('IP адрес', max_length=16)
     star = models.ForeignKey(RatingStart, on_delete=models.CASCADE, verbose_name='звезда')
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name='фильм')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='фильм')
 
     def __str__(self):
         return f"{self.star} - {self.movie}"
